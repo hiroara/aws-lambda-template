011b69432a2be2944d8d72d46a6041eb41f5c054
Arai Hiroki <hiroara62@gmail.com>
2015-06-27 17:39:55 +0900

    pass context object on test

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-require('./build/main.js').handler({ message: 'TEST' });
+var message = {
+  debug: true,
+  message: 'TEST'
+};
+
+var Q = require('q');
+(new Q.Promise(function(resolve, reject) {
+  var context = {
+    succeed: function(msg) { resolve(msg); },
+    fail: function(err) { reject(err); },
+    done: function(msg, err) {
+      if (err == null) { resolve(msg); } else { reject(err); }
+    }
+  };
+  require('./build/main.js').handler(message, context);
+})).then(function(msg) {
+  console.info("INFO: Execution complete successfully.");
+  console.info(msg);
+}).fail(function(err) {
+  console.error("ERROR: Failed to execute handler...");
+  console.error(err);
+});
