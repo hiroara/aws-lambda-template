a51c23e54022c574f60f6fbb1d8db481b0e554d4
Arai Hiroki <hiroara62@gmail.com>
2015-06-27 14:03:38 +0900

    rename dist.zip => build.zip

--- bin/create-function.sh
+++ bin/create-function.sh
@@ -8,4 +8,4 @@ INPUT_JSON=${DIR}/tmp/create-function.json
 source environment.sh
 node ${DIR}/bin/filter-config.js ${DIR}/config.json > ${INPUT_JSON}
 gulp bundle
-aws lambda create-function --cli-input-json file://${INPUT_JSON} --zip-file fileb://dist.zip
+aws lambda create-function --cli-input-json file://${INPUT_JSON} --zip-file fileb://build.zip
--- bin/update-function-code.sh
+++ bin/update-function-code.sh
@@ -8,4 +8,4 @@ INPUT_JSON=${DIR}/tmp/update-function-code.json
 source environment.sh
 node ${DIR}/bin/filter-config.js ${DIR}/config.json FunctionName > ${INPUT_JSON}
 gulp bundle
-aws lambda update-function-code --cli-input-json file://${INPUT_JSON} --zip-file fileb://dist.zip
+aws lambda update-function-code --cli-input-json file://${INPUT_JSON} --zip-file fileb://build.zip
